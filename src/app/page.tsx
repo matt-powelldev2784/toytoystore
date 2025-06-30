d25df9a64dc9a81ccc8f12c6982e7d5ca53e8601
Matthew Powell <matt.powell2784@gmail.com>
2025-06-30 13:53:47 +0100

    feat: add footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,13 @@
+import Footer from '@/components/footer'
 import Hero from '@/components/hero'
 import TopCategories from '@/components/topCategories'
 
 export default async function Home() {
   return (
-    <section className="flex flex-col items-center justify-start min-h-screen">
+    <section className="flex flex-col items-center justify-start min-h-screen min-w-screen">
       <Hero />
       <TopCategories />
-
-      <div className="">
-        <img
-          src="toyshop_logo.svg"
-          alt="teddy bear icon"
-          className="h-20 mr-2 mb-2 bg-red-500 w-full p-4"
-        />
-
-        <h2 className="text-lg font-semibold mt-6 text-center">
-          Welcome to the Toy Toy Shop!
-        </h2>
-        <p className="text-md mb-4 max-w-[800px] text-justify px-4 ht-12">
-          The Toy Toy Shop is passionate about to creating magical moments
-          through the softest, safest, and most lovable plush toys. Every toy in
-          our collection is hand-picked for its exceptional quality and ability
-          to become a cherished companion.
-        </p>
-      </div>
+      <Footer />
     </section>
   )
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-gray-300 py-4 text-center w-full min-h-40 flex items-end justify-center">
+      <p className="text-sm mb-4">
+        © 2024 The Toy Toy Shop. All rights reserved.
+      </p>
+    </footer>
+  )
+}
